ffecc1b3c4897ee746aa5ae7798d0d396f5e8cd2
sbulatao <114209345+sbulatao@users.noreply.github.com>
2025-07-08 13:06:02 -0700

    Create main.ino

---
+++ main.ino
@@ -0,0 +1,6 @@
+// For the ESP32 to flash to get data to/from Polar V3 and H10
+#include <BLEDevice.h>
+#include <Time.h> // Timekeeping functionality
+// #include <ESP32Time> // set and retrieve internal RTC time on ESP32 boards
+
+// NEED more research
